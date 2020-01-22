2bc98308b735ed493e967b773b829e2dddcaff7f
Maksym Ovrutskyi <12usa34airbornE@gmail.com>
2020-01-22 13:27:26 +0200

    test 1

--- js.js
+++ js.js
@@ -1,4 +1,23 @@
-"use strict";
+// "use strict";
 
-let g = 3;
-console.log(g);
+let money, 
+    time;
+
+money = prompt("Ваш бюджет на месяц?", '');
+console.log(money);
+time = prompt('Введите дату в формате YYYY-MM-DD', ''); 
+console.log(time);
+let question1 = prompt('Введите обязательную статью расходов в этом месяце', ''); 
+let question2 = prompt('Во сколько обойдется?', ''); 
+
+let obj = {
+    money: money,
+    timeData: time,
+    expenses: {
+        question1: question2
+    },
+    optionalExpenses: {},
+    income: {},
+    savings: false
+};
+alert("пользователя бюджет на 1 день = " + money/30);
